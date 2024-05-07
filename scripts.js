ffba5bef872342764785ecca3fdcbd393d2ac6ee
ErikBoesen <me@erikboesen.com>
2024-05-07 16:23:09 -0400

    Fix parallax

--- scripts.js
+++ scripts.js
@@ -1,10 +1,13 @@
 const parisColumns = document.querySelectorAll('.column.paris'),
       dallasColumns = document.querySelectorAll('.column.dallas');
 
-window.onscroll = function() {
+function parallax() {
     let top = document.scrollingElement.scrollTop;
     for (let i = 0; i < parisColumns.length; i++) {
-        parisColumns[i].style.backgroundPosition = '0 ' + (top * (.1)) + '%';
-        dallasColumns[i].style.backgroundPosition = '0 ' + (top * (.1)) + '%';
+        parisColumns[i].style.backgroundPosition = '0 ' + ((top - parisColumns[i].offsetTop) * .1) + '%';
+        dallasColumns[i].style.backgroundPosition = '0 ' + ((top - dallasColumns[i].offsetTop) * .1) + '%';
     }
 }
+
+window.onscroll = parallax;
+parallax();
