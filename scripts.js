9b7578800c5855934539972245eaa6d73b4f9d1d
ErikBoesen <me@erikboesen.com>
2024-05-06 22:13:41 -0400

    Tweak parallax

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ const parisColumns = document.querySelectorAll('.column.paris'),
 window.onscroll = function() {
     let top = document.scrollingElement.scrollTop;
     for (let i = 0; i < parisColumns.length; i++) {
-        parisColumns[i].style.backgroundPosition = '0 ' + (top * (-0.1)) + '%';
-        dallasColumns[i].style.backgroundPosition = '0 ' + (top * (-0.1)) + '%';
+        parisColumns[i].style.backgroundPosition = '0 ' + (top * (.1)) + '%';
+        dallasColumns[i].style.backgroundPosition = '0 ' + (top * (.1)) + '%';
     }
 }
